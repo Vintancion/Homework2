332403f64513fe21d031e138a8c3e739c6f705c3
Vintancion <Vitalikarshinov1122@gmail.com>
2024-07-19 21:57:16 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+course = 'python' 
+namber_of_hw = '12'
+time = '1.5'
+watch = '0.125'
+print('название курса:', course,',', 'всего задач:', namber_of_hw,',', 'затрачено часов:', time,',', 'среднее время выполнения:', watch)
